33d0d6b08ff36a40f6c9749af078abf38b688359
Camila Carrillo <carrillo.ca@northeastern.edu>
2026-04-19 18:30:32 -0400

    all pages implemented

--- app/src/pages/12_Ryan_View_Reviews.py
+++ app/src/pages/12_Ryan_View_Reviews.py
@@ -1,5 +1,6 @@
 import logging
 logger = logging.getLogger(__name__)
+from datetime import date
 
 import streamlit as st
 import requests
@@ -12,7 +13,7 @@ SideBarLinks()
 st.title("Reviews")
 st.subheader("View Reviews", divider="gray")
 
-rest_id = st.number_input("Enter the id of the restaurant whose reviews you'd like to view",
+rest_id = st.number_input("Enter the id of the **restaurant** whose reviews you'd like to view",
                              min_value=0, step=1)
 
 if rest_id > 0:
@@ -30,20 +31,62 @@ if rest_id > 0:
 st.subheader("Post a Review", divider="gray")
 
 with st.container(border=True):
-    stu_id = st.number_input("Please input your student id", min_value=0, step=1, key="stu_id")
-    rest_id = st.number_input("Please input the id of the restaurant you'd like to review",
-                                min_value=0, step=1, key="rest_id")
-    rating = st.number_input("Please enter your rating (1-5)", min_value=1, max_value=5, step=1)
+    review_type = st.radio("What are you reviewing?*", ["Restaurant", "Dining Hall"])
+    stu_id = st.number_input("Student id*", min_value=0, step=1, key="stu_id")
+    rating = st.radio("Rating*", [1, 2, 3, 4, 5])
+    comment = st.text_input("Comments?")
+    price_rating = st.radio("Price Rating", ["", 1, 2, 3, 4, 5])
+    allergen_flag = st.text_input("Allergen flags?")
 
-if stu_id > 0 and rest_id > 0 and rating > 0:
-    if st.button("Add a review"):
-        response = requests.post(f'http://web-api:4000/restaurants/{rest_id}/reviews',
-                    json={
+    if review_type == "Restaurant":
+        rest_id = st.number_input("Please input the id of the restaurant you'd like to review",
+                                    min_value=0, step=1, key="rest_id")
+
+        if stu_id > 0 and rest_id > 0 and rating > 0:
+            if st.button("Add a review"):
+                response = requests.post(f'http://web-api:4000/restaurants/{int(rest_id)}/reviews',
+                            json={
+                            "studentId": stu_id,
+                            "rating": rating,
+                            "comment": comment,
+                            "allergenFlag": allergen_flag,
+                            "priceRating": price_rating,
+                            "reviewDate": date.today().isoformat()
+                            },
+                            headers={"Content-Type": "application/json"}, timeout=10).json()
+                if "error" not in response:
+                    st.success("Added review successfully!")
+                else:
+                    st.error(response["error"])
+    else:
+        dh_id = st.number_input("Please input the id of the dining hall you'd like to review",
+                                min_value=0, step=1, key="dh_id")
+        if stu_id > 0 and dh_id > 0:
+            if st.button("Add a review"):
+                response = requests.post(f'http://web-api:4000/student/{int(stu_id)}/review',
+                json={
                     "studentId": stu_id,
                     "rating": rating,
-                    },
-                    headers={"Content-Type": "application/json"}, timeout=10).json()
-        if "error" not in response:
-            st.success("Added review successfully!")
-        else:
-            st.error(response["error"])
+                    "comment": comment,
+                    "allergenFlag": allergen_flag,
+                    "priceRating": price_rating,
+                    "ReviewDate": date.today().isoformat()
+                }, headers={"Content-Type": "application/json"}, timeout=10)
+                if "error" not in response:
+                    st.success("Added review successfully!")
+                else:
+                    st.error(response["error"])
+
+st.subheader("Delete Review", divider="gray")
+
+with st.container(border=True):
+    stu_id_del = st.number_input("Please enter your student id", min_value=0, step=1, key="del_stu_id")
+    review_id = st.number_input("Please enter the review id of the review you want to delete",
+                                min_value=0, step=1, key="review_id")
+    if review_id > 0 and stu_id_del > 0:
+        if st.button("Delete a review"):
+            response = requests.delete(f'http://web-api:4000/student/{int(stu_id_del)}/reviews/{int(review_id)}', timeout=10)
+            if response.status_code == 200:
+                st.success("Review deleted successfully!")
+            else:
+                st.error("Failed to delete review")
