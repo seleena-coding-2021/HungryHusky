73a8a62ab192002c7f27dc344c37f4fc010a802b
Camila Carrillo <carrillo.ca@northeastern.edu>
2026-04-19 13:16:18 -0400

    edit ryan homepage and delete commented out code

--- app/src/pages/11_Ryan_View_Restaurants.py
+++ app/src/pages/11_Ryan_View_Restaurants.py
@@ -15,25 +15,6 @@ SideBarLinks()
 st.title("Restaurants")
 st.subheader("Restaurant Filtering", divider="gray")
 
-# create a 2 column layout
-#col1, col2 = st.columns(2)
-
-# add one number input for variable 1 into column 1
-#with col1:
-#    var_01 = st.number_input("Variable 01:", step=1)
-
-# add another number input for variable 2 into column 2
-#with col2:
-#    var_02 = st.number_input("Variable 02:", step=1)
-
-# add a button to use the values entered into the number field to send to the
-# prediction function via the REST API
-#if st.button("Calculate Prediction", type="primary", use_container_width=True):
-#    logger.info(f"var_01 = {var_01}, var_02 = {var_02}")
-#    results = requests.get(f"http://web-api:4000/prediction/{var_01}/{var_02}")
-#    json_results = results.json()
-#    st.dataframe(json_results)
-
 cuisine = st.selectbox("Filter by cuisine:",
                            ["", "Italian", "American", "Chinese", "Mexican",
                             "Mediterranean", "Indian", "Fusion", "Other"])
